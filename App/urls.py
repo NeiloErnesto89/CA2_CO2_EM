7ca33662a7f2b404ec6766bda7f289b09f3991a0
patrickpulfer <patrick.pulfer1@gmail.com>
2023-02-12 16:39:05 +0000

    Change: Updated requirements & added templates + routings

--- App/urls.py
+++ App/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(core.urls)),
+    path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-handler404 = "core.views.handle_not_found"
+#handler404 = "core.views.handle_not_found"
