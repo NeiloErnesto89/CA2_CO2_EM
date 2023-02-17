39e64492664d92ca5970e3c81f482f8ed5a81019
davidryan-316 <L00177808@atu.ie>
2023-02-17 19:41:00 +0000

    Updated more unit tests

--- UnitTests/LiveFlightCarbonTest.py
+++ UnitTests/LiveFlightCarbonTest.py
@@ -1,13 +1,19 @@
 import unittest
 
+class LiveFlightCarbonTest(unittest.TestCase):
 
+    def long_haul_co2(self):
+         #Call long haul method
+         
+        self.assertEqual(850, 850)
+
+    #def short_haul_c02(self):
+    #     #Call long haul method
+    #    self.assertEqual(850, 832)
+
+        ### More methods
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
+            self.assertEqual("dottie".upper(),"DOTTIE")
 
 if __name__ == '__main__':
     unittest.main()
