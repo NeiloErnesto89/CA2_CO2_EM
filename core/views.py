9cae0bba21c1d99a0b8b4fdd9b1eae811ca072f2
Patrick Pulfer <Patrick.Pulfer1@gmail.com>
2023-02-18 15:23:25 +0000

    Add: Initial folder structure (#9)

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from core.Services import *
 
 
 def home(request):
@@ -7,3 +8,21 @@ def home(request):
 
 def q1(request):
     return render(request, 'core/q1.html')
+
+def q2(request):
+    return render(request, 'core/q2.html')
+
+def q3(request):
+    return render(request, 'core/q3.html')
+
+def q4(request):
+    return render(request, 'core/q4.html')
+
+def q5(request):
+    return render(request, 'core/q5.html')
+
+def q6(request):
+    return render(request, 'core/q6.html')
+
+def q7(request):
+    return render(request, 'core/q7.html')
